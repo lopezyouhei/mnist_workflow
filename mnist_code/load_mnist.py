e0df84eef4155d026160585c849cfb765e51f7d5
Enrique <lopezyouhei@gmail.com>
2023-10-10 21:58:48 +0200

    separate load_raw function into 2 functions: load_raw and load_tensor

--- mnist_code/load_mnist.py
+++ mnist_code/load_mnist.py
@@ -9,25 +9,40 @@ class LoadMNIST():
         PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
         self.data_folder = os.path.join(PROJECT_ROOT, 'data/')
 
-    def load_raw_mnist(self):
-        transform = transforms.ToTensor()
+    def load_raw(self, transform=None):
+        """The function downloads (if necessary) and returns the train
+        and test MNIST datasets.
+
+        Returns:
+            _type_: _description_
+        """
         raw_data = os.path.join(self.data_folder, 'raw/')
         trainset = datasets.MNIST(root=raw_data,
                                   train=True, 
                                   download=True, 
                                   transform=transform)
-        train_loader = torch.utils.data.DataLoader(trainset,
-                                                   batch_size=64,
-                                                   shuffle=True)
+        
 
         testset = datasets.MNIST(root=raw_data,
                                  train=False,
                                  download=True,
                                  transform=transform)
         
-        test_loader = torch.utils.data.DataLoader(testset,
-                                                  batch_size=64,
+        
+        
+        return trainset, testset
+    
+    def load_tensor(self, batch_size=32):
+
+        train_set, test_set = self.load_raw(transform=transforms.ToTensor())
+
+        train_loader = torch.utils.data.DataLoader(train_set,
+                                                   batch_size=batch_size,
+                                                   shuffle=True)
+        test_loader = torch.utils.data.DataLoader(test_set,
+                                                  batch_size=batch_size,
                                                   shuffle=True)
         
         return train_loader, test_loader
+
 
