3293ae0f4ecc5a095a8a6849e3d7f86f1ffea4c7
Enrique <lopezyouhei@gmail.com>
2023-10-11 07:37:13 +0200

    fix 2 bugs in visualize_mnist function

--- mnist_code/helper_functions.py
+++ mnist_code/helper_functions.py
@@ -4,15 +4,15 @@ import math
 def visualize_mnist(loader, num_images=8):
 
     dataiter = iter(loader)
-    images, labels = dataiter.next()
+    images, labels = next(dataiter)
 
     fig, axes = plt.subplots(nrows=2, 
                              ncols=math.ceil(num_images/2),
                              figsize=(10,2))
     
-    for i, ax in enumerate(axes):
+    for i, ax in enumerate(axes.ravel()):
         ax.imshow(images[i].squeeze().numpy(), cmap='gray')
-        ax.title.set_text(str(labels[i].item()))
+        ax.title.set_text("Label:" + str(labels[i].item()))
         ax.axis('off')
     
     plt.tight_layout()
